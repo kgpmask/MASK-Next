d5bedbb73448e9b695f4c9518d32ac54272f902e
Jeffrey Samuel <jeffrey.samuel@protonmail.com>
2023-12-21 22:40:20 +0530

    Tabs instead of spaces

--- utils/database/dbInit.ts
+++ utils/database/dbInit.ts
@@ -5,19 +5,19 @@ const MONGO_URL = process.env.MONGO_URL
 if (!MONGO_URL) throw new Error("MONGO_URL is not defined.");
 
 const dbConnect = async () => {
-  const opts = {
-    connectTimeoutMS: 5000
-  }
+	const opts = {
+		connectTimeoutMS: 5000
+	}
 
-  try {
-    const db = await mongoose.connect(MONGO_URL, opts); // insert additional checks here
+	try {
+		const db = await mongoose.connect(MONGO_URL, opts); // insert additional checks here
 
-    // insert additional checks here
+		// insert additional checks here
 
-    return db;
-  } catch (e) {
-    throw e
-  }
+		return db;
+	} catch (e) {
+		throw e
+	}
 };
 
 export default dbConnect
--- utils/models/test_model.ts
+++ utils/models/test_model.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 export interface IMember extends mongoose.Document {
-  name: string;
-  email: string;
-  roles?: string[];
+	name: string;
+	email: string;
+	roles?: string[];
 };
 
 export const memberSchema = new mongoose.Schema<IMember>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  roles: { type: [String] },
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	roles: { type: [String] },
 })
 
 const Member = mongoose.model<IMember>('Member', memberSchema);
