2f1305b8b3ee6a53223a1205e99a83cfb4255f2e
Jeffrey Samuel <jeffrey.samuel@protonmail.com>
2023-12-22 20:23:19 +0530

    Final changes before merge

--- utils/database/dbInit.ts
+++ utils/database/dbInit.ts
@@ -1,23 +1,21 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
 
 if (!MONGO_URL) throw new Error("MONGO_URL is not defined.");
 
 const dbConnect = async () => {
 	const opts = {
-		connectTimeoutMS: 5000
-	}
+		connectTimeoutMS: 5000,
+	};
 
 	try {
 		const db = await mongoose.connect(MONGO_URL, opts); // insert additional checks here
 
-		// insert additional checks here
-
 		return db;
 	} catch (e) {
-		throw e
+		throw e;
 	}
 };
 
-export default dbConnect
+export default dbConnect;
