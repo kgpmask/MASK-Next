c7c15ff9d3c3c8395fd7f6fd678fcb6abab9b0e3
Animesh (Bakasur) <139889836+wildcraft958@users.noreply.github.com>
2023-12-24 15:10:55 +0530

    Carousel_home_page (#5)

---
+++ components/home-page/Banner.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '@/styles/Home.module.css';
+import Image from 'next/image';
+
+
+
+function Banner() {
+    const eventAlert = null;
+  return (
+    <div>
+      <div className={styles["mask-banner"]}>
+				<Image className={styles["mask-banner-image"]} src="/transparent_logo.png" alt="MASK" width={120} height={120} />
+				<span className={styles["mask-banner-text-container"]}><span className={styles["mask-banner-text"]}>Manga and Anime Society Kharagpur</span></span>
+				<span className={styles["mask-banner-edge"]}>&nbsp;</span>
+			</div>
+			{eventAlert && (<div className={styles["event-alert"]} style={{ margin: "50px 0px" }}>
+				{eventAlert}
+			</div>)}
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/home-page/Updates.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { MdFiberNew } from 'react-icons/md';
+import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
+
+interface Update {
+  name: string;
+  link: string;
+  type: string;
+  date?: Date;
+  hype?: boolean;
+}
+
+interface UpdatesProps {
+  updates: Update[];
+}
+
+const Updates: React.FC<UpdatesProps> = ({ updates }) => {
+  return (
+    <div className={styles['notices']}>
+      <div className={styles['noticebox']}>
+        <div className={styles['notices-header']}>Updates!</div>
+        <div className={styles['notices-list']}>
+          {updates
+            .filter(({ hype }) => hype)
+            .map(({ name, link, type, date }, index) => (
+              <React.Fragment key={link}>
+                {index > 0 && <hr className={styles['notice-break']} />}
+                <Link href={(type === 'art' ? '/art/' : '') + link} className={styles['notice-link']}>
+                  <div className={styles['notice-post']}>
+                    {name}
+                    {date && Date.now() - date.getTime() < 7 * 24 * 60 * 60 * 1000 && (
+                      <MdFiberNew size={16} color='var(--error-red)' style={{ marginLeft: 4 }} />
+                    )}
+                  </div>
+                </Link>
+              </React.Fragment>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Updates;
+
+
