824c042ab08be24e71f6bbed3bbbe5cb87c967c6
Animesh <animeshraj958@gmail.com>
2023-12-25 18:42:08 +0530

    fixed idententions

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { BiLogoFacebookSquare, BiLogoGmail, BiLogoGithub, BiLogoInstagram, BiLogoYoutube } from "react-icons/bi";
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { BiLogoFacebookSquare, BiLogoGmail, BiLogoGithub, BiLogoInstagram, BiLogoYoutube } from 'react-icons/bi';
 
-import SocialHandles from "./SocialHandles";
-import SocietyLogo from "./SocietyLogo";
+import SocialHandles from './SocialHandles';
+import SocietyLogo from './SocietyLogo';
 
 const Footer = () => {
 	const footerLinks = [
 		{
-			name: "YouTube",
-			href: "https://www.youtube.com/@maskiitkgp",
-			icon: BiLogoYoutube,
+			name: 'YouTube',
+			href: 'https://www.youtube.com/@maskiitkgp',
+			icon: BiLogoYoutube
 		},
 		{
-			name: "Instagram",
-			href: "https://www.instagram.com/maskiitkgp",
-			icon: BiLogoInstagram,
+			name: 'Instagram',
+			href: 'https://www.instagram.com/maskiitkgp',
+			icon: BiLogoInstagram
 		},
 		{
-			name: "Facebook",
-			href: "https://www.facebook.com/maskiitkgp",
-			icon: BiLogoFacebookSquare,
+			name: 'Facebook',
+			href: 'https://www.facebook.com/maskiitkgp',
+			icon: BiLogoFacebookSquare
 		},
-		{ name: "Gmail", href: "mailto:kgpmask@gmail.com", icon: BiLogoGmail },
+		{ name: 'Gmail', href: 'mailto:kgpmask@gmail.com', icon: BiLogoGmail },
 		{
-			name: "GitHub",
-			href: "https://github.com/kgpmask/MASK",
-			icon: BiLogoGithub,
-		},
+			name: 'GitHub',
+			href: 'https://github.com/kgpmask/MASK',
+			icon: BiLogoGithub
+		}
 	];
 
 	return (
@@ -44,9 +44,8 @@ const Footer = () => {
 			<div id="contact-us">
 				<span className="float-middle">Find us!</span>
 			</div>
-			{footerLinks.map(({ name, href, icon }) => (
-				<SocialHandles title={name} link={href} Icon={icon} key={href} />
-			))}
+			{footerLinks.map( ({ name, href, icon }) => 
+				<SocialHandles title={name} link={href} Icon={icon} key={href} /> )}
 		</footer>
 	);
 };
