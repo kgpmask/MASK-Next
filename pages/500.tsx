6fc5bf8ff26a46a28ada93c451587a1b6a846cd9
Animesh <animeshraj958@gmail.com>
2023-12-25 18:00:56 +0530

    done with 404 and 500

---
+++ pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500 () {
+	return (
+		<>
+			<h1>Uh Oh!!! Something went wrong at our end.</h1>
+			<h3> Don&apos;t worry, It&apos;s not you :) It&apos;s Us :( , sorry about that </h3>
+		</>
+	);
+}
