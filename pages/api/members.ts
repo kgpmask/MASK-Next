9399837c789ab39919a31e32b2d84b6500d3abf6
Signor-Koala <jeffrey.samuel@protonmail.com>
2023-12-26 10:54:28 +0530

    Merged getMembersByYear into the API directly

--- pages/api/members.ts
+++ pages/api/members.ts
@@ -1,13 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../../utils/database/dbInit';
-import { getMembersByYears } from '@/utils/database/dbHandler';
+import dbConnect from '@/utils/database/dbInit';
+import Member, { IMember, MemberType } from '@/utils/models/Member';
 
 export default async function handler (
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	const _ = await dbConnect();
-	const m = await getMembersByYears( Number( req.query.year ) );
-	res.status( 200 ).json( JSON.stringify( m ) );
+	await dbConnect();
+
+	const members: Array<MemberType> = [];
+
+	const data: Array<IMember> = await Member.find({
+		'records.year': ~~Number( req.query.year )
+	});
+
+	data.forEach( ( member ) => {
+		const rec = member.records.find(
+			( rec ) => rec.year === Number( req.query.year )
+		);
+		console.log( rec );
+		if ( rec ) {
+			members.push({
+				name: member.name,
+				image: member.image,
+				position: rec.position,
+				teams: rec.position
+			});
+		}
+	});
+	res.status( 200 ).json( JSON.stringify( members ) );
 }
