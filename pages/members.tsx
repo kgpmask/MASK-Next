2bb8d352278bc0149146f57bbc05e076e6645834
Signor-Koala <jeffrey.samuel@protonmail.com>
2023-12-28 13:56:06 +0530

    Trying to make the members frontend
    I've been breaking my head over this, mostly due to the fact that i understand nothing of my own code, and partly due to the fact that only have a vague understanding of how react does things. I really need some help with this, since im clearly not cut out for programming ;-;

--- pages/members.tsx
+++ pages/members.tsx
@@ -11,58 +11,69 @@ import HeadContent from '@/components/HeadContent';
 import styles from '@/styles/Members.module.css';
 import Member from '@/components/Member';
 import { MemberType } from '@/utils/models/Member';
+import { useEffect, useState } from 'react';
 
-export type ProfileType = {
+type TeamType = {
   teamColor: string;
-  teamName: string;
+  teamIcon: string;
 };
 
 type MemberProfile = {
   name: string;
   imageLink: string;
-  teams: ProfileType[];
+  position: string;
+  teams: TeamType[];
 };
 
-export default function MembersPage () {
-	// const temp = await fetch('/api/members?year=2023');
-	// The above doesnt work since making this function async breaks it
-	// So for now, we are taking the data from @/utils/samples/members.json
-	// The format is assumed to be json with the following fields:
-	// > name
-	// > image
-	// > position
-	// > [teams]
-	// [teams] is a list of all the teams along with the position in said teams
-	// the format of each element in teams is: <'S'/'H'/''> + '_' + <team_name>
-	// position should always be singular in the backend
-	const data: Array<MemberType> = require('../utils/samples/members.json');
+// export type MemberType = {
+//   name: string;
+//   image: string;
+//   position: string;
+//   teams: string[];
+// };
 
-	const status: Record<string, Array<MemberProfile>> = {
-		Governor: [],
-		'Team Heads': [],
-		'Team Sub-Heads': [],
-		Advisors: [],
-		'Research Associates': [],
-		Executives: [],
-		Associates: [],
-		Freshers: [],
-		'Former Members': []
-	};
-	data.forEach((member) => {
-		const teamProfles: Array<ProfileType> = [];
-		member.teams.forEach((team) => {
-			const [pos, teamName] = team.split('_');
-			const teamColor =
-        pos === 'H' ? 'yellow' : pos === 'S' ? 'green' : 'white';
-			teamProfles.push( { teamName, teamColor } );
-		} );
-		status[member.position].push( {
-			name: member.name,
-			imageLink: member.image,
-			teams: teamProfles
-		} );
-	} );
-	console.log(data);
+export default function MembersPage () {
+	const [personsRecord, setPersonsRecord] = useState<MemberProfile[]>([]);
+	const [yearRecord, setYearRecord] = useState<Number>(2023);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const temp = await fetch(`/api/members?year=${yearRecord}`);
+				const data: MemberType[] = await temp.json();
+				// Start
+				const memberArray: MemberProfile[] = [];
+				data.forEach((person) => {
+					const teamsArray: TeamType[] = [];
+					person.teams.forEach((team) => {
+						teamsArray.push( {
+							teamColor:
+                team[1] === 'H'
+                	? 'yellow'
+                	: team[1] === 'S'
+                		? 'green'
+                		: 'white',
+							teamIcon: team[0]
+						} );
+					} );
+					memberArray.push( {
+						name: person.name,
+						imageLink: person.image,
+						position: person.position,
+						teams: teamsArray
+					} );
+				} );
+				setPersonsRecord(memberArray);
+				// End
+			} catch (e) {
+				console.log(e);
+			}
+		};
+		fetchData();
+	}, [yearRecord]);
+
+	console.log(personsRecord);
+
 	const records = {
 		'Team Heads': [
 			{
