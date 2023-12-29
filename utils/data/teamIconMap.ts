1317a6c365eda3392e3100564b2d29055c6a1019
Goose-Win11 <64352864+Goose-Of-War@users.noreply.github.com>
2023-12-29 19:28:14 +0530

    That's all there was to it (and Quiz Icon Change)

--- utils/data/teamIconMap.ts
+++ utils/data/teamIconMap.ts
@@ -1,5 +1,5 @@
 import { IconType } from 'react-icons';
-import { GiChecklist, GiLaptop, GiMusicalNotes, GiPaintBrush, GiPencil, GiTv } from 'react-icons/gi';
+import { GiThink, GiLaptop, GiMusicalNotes, GiPaintBrush, GiPencil, GiTv } from 'react-icons/gi';
 
 type IconMapType = Record<string, Record<string, { name: string; icon: IconType }>>;
 
@@ -7,28 +7,28 @@ const iconMap: IconMapType = {
 	2020: {
 		a: { name: 'AMV', icon: GiTv },
 		c: { name: 'Creative', icon: GiPaintBrush },
-		q: { name: 'Quiz', icon: GiChecklist },
+		q: { name: 'Quiz', icon: GiThink },
 		w: { name: 'WebDev', icon: GiLaptop },
 	},
 	2021: {
 		a: { name: 'AMV', icon: GiTv },
 		d: { name: 'Design & Arts', icon: GiPaintBrush },
 		m: { name: 'Music', icon: GiMusicalNotes },
-		q: { name: 'Quiz', icon: GiChecklist },
+		q: { name: 'Quiz', icon: GiThink },
 		w: { name: 'WebDev', icon: GiLaptop },
 	},
 	2022: {
 		a: { name: 'AMV & Music', icon: GiTv },
 		d: { name: 'Design & Arts', icon: GiPaintBrush },
 		n: { name: 'Media & Newsletter', icon: GiPencil },
-		q: { name: 'Quiz', icon: GiChecklist },
+		q: { name: 'Quiz', icon: GiThink },
 		w: { name: 'WebDev', icon: GiLaptop },
 	},
 	2023: {
 		a: { name: 'AMV & Music', icon: GiTv },
 		d: { name: 'Design & Arts', icon: GiPaintBrush },
 		n: { name: 'Media & Newsletter', icon: GiPencil },
-		q: { name: 'Quiz', icon: GiChecklist },
+		q: { name: 'Quiz', icon: GiThink },
 		w: { name: 'WebDev', icon: GiLaptop },
 	},
 };
