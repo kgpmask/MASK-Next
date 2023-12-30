7434f1666ba5e1b043462f216695b69522fa2637
Symbiot01 <sahil0111patel@gmail.com>
2023-12-30 23:56:52 +0530

    lint

--- pages/art.tsx
+++ pages/art.tsx
@@ -14,237 +14,238 @@ interface ArtImage {
 }
 
 const getRandomImageURL = () => {
-  const randomWidth = Math.floor(Math.random() * 400) + 200; // Random width between 200 and 600
-  const randomHeight = Math.floor(Math.random() * 400) + 200; // Random height between 200 and 600
-  return `https://picsum.photos/${randomWidth}/${randomHeight}`;
+	const randomWidth = Math.floor(Math.random() * 400) + 200; // Random width between 200 and 600
+	const randomHeight = Math.floor(Math.random() * 400) + 200; // Random height between 200 and 600
+	return `https://picsum.photos/${randomWidth}/${randomHeight}`;
 };
 
 const artData: ArtImage[] = [
-  {
-    "name": "Art - Tanjiro Kamado",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Sanjeev Raj Ganji"],
-    "date": "2021-08-29T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Saitama",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Garima Mendhe"],
-    "date": "2021-08-13T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Kirigakure Shinobi Massacre",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Arpit Das"],
-    "date": "2020-05-12T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Garou",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Pritam Mallick"],
-    "date": "2019-10-28T00:00:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Tanjiro Kamado",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Sanjeev Raj Ganji"],
-    "date": "2021-08-29T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Saitama",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Garima Mendhe"],
-    "date": "2021-08-13T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Kirigakure Shinobi Massacre",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Arpit Das"],
-    "date": "2020-05-12T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Garou",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Pritam Mallick"],
-    "date": "2019-10-28T00:00:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Tanjiro Kamado",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Sanjeev Raj Ganji"],
-    "date": "2021-08-29T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Saitama",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Garima Mendhe"],
-    "date": "2021-08-13T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Kirigakure Shinobi Massacre",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Arpit Das"],
-    "date": "2020-05-12T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Garou",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Pritam Mallick"],
-    "date": "2019-10-28T00:00:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Tanjiro Kamado",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Sanjeev Raj Ganji"],
-    "date": "2021-08-29T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Saitama",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Garima Mendhe"],
-    "date": "2021-08-13T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Kirigakure Shinobi Massacre",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Arpit Das"],
-    "date": "2020-05-12T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Garou",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Pritam Mallick"],
-    "date": "2019-10-28T00:00:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Tanjiro Kamado",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Sanjeev Raj Ganji"],
-    "date": "2021-08-29T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Saitama",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Garima Mendhe"],
-    "date": "2021-08-13T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Kirigakure Shinobi Massacre",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Arpit Das"],
-    "date": "2020-05-12T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Garou",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Pritam Mallick"],
-    "date": "2019-10-28T00:00:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Tanjiro Kamado",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Sanjeev Raj Ganji"],
-    "date": "2021-08-29T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Saitama",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Garima Mendhe"],
-    "date": "2021-08-13T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Kirigakure Shinobi Massacre",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Arpit Das"],
-    "date": "2020-05-12T18:30:00.000Z",
-    "hype": true
-  },
-  {
-    "name": "Art - Garou",
-    "link": getRandomImageURL(),
-    "type": "art",
-    "attr": ["Pritam Mallick"],
-    "date": "2019-10-28T00:00:00.000Z",
-    "hype": true
-  },
+	{
+		'name': 'Art - Tanjiro Kamado',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Sanjeev Raj Ganji'],
+		'date': '2021-08-29T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Saitama',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Garima Mendhe'],
+		'date': '2021-08-13T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Kirigakure Shinobi Massacre',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Arpit Das'],
+		'date': '2020-05-12T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Garou',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Pritam Mallick'],
+		'date': '2019-10-28T00:00:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Tanjiro Kamado',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Sanjeev Raj Ganji'],
+		'date': '2021-08-29T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Saitama',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Garima Mendhe'],
+		'date': '2021-08-13T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Kirigakure Shinobi Massacre',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Arpit Das'],
+		'date': '2020-05-12T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Garou',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Pritam Mallick'],
+		'date': '2019-10-28T00:00:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Tanjiro Kamado',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Sanjeev Raj Ganji'],
+		'date': '2021-08-29T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Saitama',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Garima Mendhe'],
+		'date': '2021-08-13T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Kirigakure Shinobi Massacre',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Arpit Das'],
+		'date': '2020-05-12T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Garou',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Pritam Mallick'],
+		'date': '2019-10-28T00:00:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Tanjiro Kamado',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Sanjeev Raj Ganji'],
+		'date': '2021-08-29T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Saitama',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Garima Mendhe'],
+		'date': '2021-08-13T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Kirigakure Shinobi Massacre',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Arpit Das'],
+		'date': '2020-05-12T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Garou',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Pritam Mallick'],
+		'date': '2019-10-28T00:00:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Tanjiro Kamado',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Sanjeev Raj Ganji'],
+		'date': '2021-08-29T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Saitama',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Garima Mendhe'],
+		'date': '2021-08-13T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Kirigakure Shinobi Massacre',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Arpit Das'],
+		'date': '2020-05-12T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Garou',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Pritam Mallick'],
+		'date': '2019-10-28T00:00:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Tanjiro Kamado',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Sanjeev Raj Ganji'],
+		'date': '2021-08-29T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Saitama',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Garima Mendhe'],
+		'date': '2021-08-13T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Kirigakure Shinobi Massacre',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Arpit Das'],
+		'date': '2020-05-12T18:30:00.000Z',
+		'hype': true
+	},
+	{
+		'name': 'Art - Garou',
+		'link': getRandomImageURL(),
+		'type': 'art',
+		'attr': ['Pritam Mallick'],
+		'date': '2019-10-28T00:00:00.000Z',
+		'hype': true
+	}
 ];
 
 const ArtPage: React.FC = () => {
-  return (
-    <>
-      <HeadContent title='Art' description='View some amazing artwork created by our DNA team. From traditional to digital to even unconvential, we have it all.' />
-      <TextPage title='Art'>
+	return (
+		<>
+			<HeadContent title='Art' description='View some amazing artwork created by our DNA team.
+       From traditional to digital to even unconvential, we have it all.' />
+			<TextPage title='Art'>
 				<p className={styles['insta-promotion']}>
           Check out our content on here or on{' '}
-					<Link  isRed href='https://www.instagram.com/maskiitkgp'>
+					<Link isRed href='https://www.instagram.com/maskiitkgp'>
             Instagram
 					</Link>
-					!
+          !
 				</p>
 			</TextPage>
-      <section id="photos" className={styles['photos']}>
-        {artData.map((img, index) => (
-          <div className={styles['imgContainer']} key={index}>
-            <img
-              id={`img-${index}`}
-              src={img.link}
-              loading="lazy"
-              alt={img.name}
-            />
-            <div className={styles['overlay']}>
-              <h1>{img.name.replace('Art - ', '')}</h1>
-              <h3>{img.attr.join(', ')}</h3>
-            </div>
-          </div>
-        ))}
-      </section>
-    </>
-  );
+			<section id="photos" className={styles['photos']}>
+				{artData.map((img, index) => 
+					<div className={styles['imgContainer']} key={index}>
+						<img
+							id={`img-${index}`}
+							src={img.link}
+							loading="lazy"
+							alt={img.name}
+						/>
+						<div className={styles['overlay']}>
+							<h1>{img.name.replace('Art - ', '')}</h1>
+							<h3>{img.attr.join(', ')}</h3>
+						</div>
+					</div>
+				)}
+			</section>
+		</>
+	);
 };
 
 export default ArtPage;
--- pages/videos.tsx
+++ pages/videos.tsx
@@ -19,67 +19,65 @@ interface Props {
   videos?: Video[];
 }
 
-const Videos: React.FC<Props> = ({ videos = [] }) => {
-  const sampleVideos: Video[] = [
-    {
-      "name": "How to get into MASK",
-      "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      "type": "youtube",
-      "attr": [
-        "Parth Mane"
-      ],
-      "date": "2009-10-24T18:30:00.000Z",
-      "hype": true
-    },
-    {
-      "name": "「AMV」The Garden of Words - A Thousand Years",
-      "link": "https://www.youtube.com/watch?v=9W4eyQ7LP7g",
-      "type": "youtube",
-      "attr": [
-        "Hrishabh Kumar Tundwar"
-      ],
-      "date": "2023-01-09T18:30:00.000Z",
-      "hype": true
-    },
-    {
-      "name": "「AMV」Assassination Classroom - Heathens",
-      "link": "https://www.youtube.com/watch?v=unITcghHNVI",
-      "type": "youtube",
-      "attr": [
-        "Chiranjeet Mishra"
-      ],
-      "date": "2023-01-09T18:30:00.000Z",
-      "hype": true
-    },
-  ];
+const Videos: React.FC<Props> = ( { videos = [] } ) => {
+	const sampleVideos: Video[] = [
+		{
+			'name': 'How to get into MASK',
+			'link': 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+			'type': 'youtube',
+			'attr': ['Parth Mane'],
+			'date': '2009-10-24T18:30:00.000Z',
+			'hype': true
+		},
+		{
+			'name': '「AMV」The Garden of Words - A Thousand Years',
+			'link': 'https://www.youtube.com/watch?v=9W4eyQ7LP7g',
+			'type': 'youtube',
+			'attr': ['Hrishabh Kumar Tundwar'],
+			'date': '2023-01-09T18:30:00.000Z',
+			'hype': true
+		},
+		{
+			'name': '「AMV」Assassination Classroom - Heathens',
+			'link': 'https://www.youtube.com/watch?v=unITcghHNVI',
+			'type': 'youtube',
+			'attr': ['Chiranjeet Mishra'],
+			'date': '2023-01-09T18:30:00.000Z',
+			'hype': true
+		}
+	];
 
-  const displayedVideos = videos && videos.length ? videos : sampleVideos;
+	const displayedVideos = videos && videos.length ? videos : sampleVideos;
 
-  return (
-    <>
-      <HeadContent title='Videos' description="Experience the thrill of captivating AMVs created by our talented team at Manga and Anime Society Kharagpur [MASK]. Immerse yourself in a world of dynamic video editing, mesmerizing soundtracks, and breathtaking visuals." />
-      <TextPage title='Videos'>
+	return (
+		<>
+			<HeadContent
+				title='Videos'
+				description='Experience the thrill of captivating AMVs created by our talented team at
+         Manga and Anime Society Kharagpur [MASK]. Immerse yourself in a world of dynamic video editing,
+          mesmerizing soundtracks, and breathtaking visuals.' />
+			<TextPage title='Videos'>
 				<p className={styles['youtube-promotion']}>
-        Check out our video content here or on {' '}
+          Check out our video content here or on {' '}
 					<Link isRed href='https://www.youtube.com/@maskiitkgp'>
             Youtube
 					</Link>
-					!
+          !
 				</p>
 			</TextPage>
-      <div className={styles['youtube-videos']}>
-        {displayedVideos.map((video, index) => (
-          <div className={styles['youtube-vid']} key={index}>
-            <LiteYouTubeEmbed
-              id={video.link.split('v=')[1]}
-              title={video.name}
-              wrapperClass={styles['lite-yt-embed']}
-            />
-          </div>
-        ))}
-      </div>
-    </>
-  );
+			<div className={styles['youtube-videos']}>
+				{displayedVideos.map((video, index) =>
+					<div className={styles['youtube-vid']} key={index}>
+						<LiteYouTubeEmbed
+							id={video.link.split('v=')[1]}
+							title={video.name}
+							wrapperClass={styles['lite-yt-embed']}
+						/>
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default Videos;
