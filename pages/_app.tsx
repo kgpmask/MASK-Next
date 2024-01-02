20c7fdcca36a4f75d478201030c21b7aa2ac2b1c
Jeffrey Samuel <jeffrey.samuel@protonmail.com>
2024-01-02 11:02:06 +0530

    modified _app.tsx to allow pages to use the authenticaion session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import '@/styles/globals.css';
 import '@/styles/layout.css';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import Layout from '@/components/Layout';
 
-export default function App ( { Component, pageProps }: AppProps) {
+export default function App ( {
+	Component,
+	pageProps: { session, ...pageProps }
+}: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<SessionProvider session={session}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</SessionProvider>
 	);
 }
