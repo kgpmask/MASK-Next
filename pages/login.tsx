d71e44fe1706da3a0e74ff9ad3dff9a02d76e821
Jeffrey Samuel <jeffrey.samuel@protonmail.com>
2024-01-04 18:45:20 +0530

    added a title

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,7 @@ import { signIn } from 'next-auth/react';
 export default function LoginPage () {
 	return (
 		<>
-			<h1>It doesn&apos;t look like this page exists! </h1>
+			<h1> Login page </h1>
 			<button onClick={() => signIn('google')}>
 				<span>Sign in</span>
 			</button>
