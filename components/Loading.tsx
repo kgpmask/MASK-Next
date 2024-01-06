b8444d30effa861d250a733a9ea4f0dff014d8db
destryptor <sharanya.chakraborty@kgpian.iitkgp.ac.in>
2024-01-06 15:08:20 +0530

    Loading page improved

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Loading: React.FC = () => {
+	const [dots, setDots] = useState(1);
+	const [showBadNetMessage, setShowBadNetMessage] = useState(false);
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setDots((prevDots) => prevDots < 3 ? prevDots + 1 : 1);
+		}, 500);
+
+		const timeoutId = setTimeout(() => {
+			setShowBadNetMessage(true);
+		}, 20000);
+
+		return () => {
+			clearInterval(intervalId);
+			clearTimeout(timeoutId);
+		};
+	}, []);
+
 	const containerStyle: React.CSSProperties = {
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center'
 	};
+
 	return (
-		<div style={containerStyle}>
-			<h1>Loading...&lt; &gt;_&lt; &gt;</h1>
-		</div>
+		<>
+			<div style={containerStyle}>
+				<h1>
+					{' '}
+					Loading{'.'.repeat(dots)}(*^‿^*)<br></br>
+					{showBadNetMessage && <i style={{ fontSize: '50%', color: 'var(--off-white)' }}>
+						Taking too much time? Check your internet connection.
+					</i>}
+				</h1>
+			</div>
+		</>
 	);
 };
 
