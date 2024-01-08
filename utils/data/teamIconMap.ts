ad0bff5c79ed4ac3e3a99f9f88fb13e5c2a3fc12
Jeffrey Samuel <jeffrey.samuel@protonmail.com>
2024-01-08 21:35:47 +0530

    Fixed more lint issues

--- utils/data/teamIconMap.ts
+++ utils/data/teamIconMap.ts
@@ -1,36 +1,46 @@
 import { IconType } from 'react-icons';
-import { GiThink, GiLaptop, GiMusicalNotes, GiPaintBrush, GiPencil, GiTv } from 'react-icons/gi';
+import {
+	GiThink,
+	GiLaptop,
+	GiMusicalNotes,
+	GiPaintBrush,
+	GiPencil,
+	GiTv
+} from 'react-icons/gi';
 
-type IconMapType = Record<string, Record<string, { name: string; icon: IconType }>>;
+type IconMapType = Record<
+  string,
+  Record<string, { name: string; icon: IconType }>
+>;
 
 const iconMap: IconMapType = {
 	2020: {
 		a: { name: 'AMV', icon: GiTv },
 		c: { name: 'Creative', icon: GiPaintBrush },
 		q: { name: 'Quiz', icon: GiThink },
-		w: { name: 'WebDev', icon: GiLaptop },
+		w: { name: 'WebDev', icon: GiLaptop }
 	},
 	2021: {
 		a: { name: 'AMV', icon: GiTv },
 		d: { name: 'Design & Arts', icon: GiPaintBrush },
 		m: { name: 'Music', icon: GiMusicalNotes },
 		q: { name: 'Quiz', icon: GiThink },
-		w: { name: 'WebDev', icon: GiLaptop },
+		w: { name: 'WebDev', icon: GiLaptop }
 	},
 	2022: {
 		a: { name: 'AMV & Music', icon: GiTv },
 		d: { name: 'Design & Arts', icon: GiPaintBrush },
 		n: { name: 'Media & Newsletter', icon: GiPencil },
 		q: { name: 'Quiz', icon: GiThink },
-		w: { name: 'WebDev', icon: GiLaptop },
+		w: { name: 'WebDev', icon: GiLaptop }
 	},
 	2023: {
 		a: { name: 'AMV & Music', icon: GiTv },
 		d: { name: 'Design & Arts', icon: GiPaintBrush },
 		n: { name: 'Media & Newsletter', icon: GiPencil },
 		q: { name: 'Quiz', icon: GiThink },
-		w: { name: 'WebDev', icon: GiLaptop },
-	},
+		w: { name: 'WebDev', icon: GiLaptop }
+	}
 };
 
 export default iconMap;
