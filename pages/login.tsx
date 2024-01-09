c3ad8d3650fc65208d4c7fc47da3fc4984349952
Jeffrey Samuel <jeffrey.samuel@protonmail.com>
2024-01-09 12:37:02 +0530

    Added a signout button

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,20 @@
-import { signIn } from 'next-auth/react';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function LoginPage () {
+	const { status } = useSession();
+	const isLoggedIn = status === 'authenticated';
 	return (
 		<>
 			<h1> Login page </h1>
-			<button onClick={() => signIn('google')}>
-				<span>Sign in</span>
-			</button>
+			{isLoggedIn ? 
+				<button onClick={() => signOut()}>
+					<span>Sign out</span>
+				</button>
+				: 
+				<button onClick={() => signIn('google')}>
+					<span>Sign in</span>
+				</button>
+			}
 		</>
 	);
 }
