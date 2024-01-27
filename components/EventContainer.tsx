296ab4d4bb0847bf27698ac5950b44eb1f708e9a
Ankan <93087057+ItsAnkan@users.noreply.github.com>
2024-01-27 13:37:16 +0530

    just some lint stuff

--- components/EventContainer.tsx
+++ components/EventContainer.tsx
@@ -1,33 +1,35 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import TextPage from './TextPage';
 interface EventProps {
-  title: string;
-  date: string;
-  venue: string;
-  author: string;
-  description: string;
-  link: string;
+	title: string;
+	date: string;
+	venue: string;
+	author: string;
+	description: string;
+	link: string;
 }
 
-const EventContainer: React.FC<EventProps> = ({ title,date,venue,author,description,link }) => {
-    return (<TextPage>
+const EventContainer: React.FC<EventProps> = ( { title, date, venue, author, description, link } ) => {
+	return <TextPage>
 		<div className="event-container">
-		<a href={`/events/${link}`} title={title}><div className='image-container'>
-			<img src={`/assets/events/${link }.jpg`} alt={title}>
-		</div></a>
-		<div className="details">
-			<h2> <a href={`/events/${link}`}  title={title }>{ title } </a></h2>
-			<p>
-				<span> Date: </span> { date } <br/>
-				<span> Venue: </span> { venue } <br/>
-				<span> Author: </span> { author }
-			</p>
-			<p> { description } </p>
-			<br/>
-			<a href={`/events/${link}`}  title={ title }> Read More... </a>
+			<a href={`/events/${ link}`} title={ title }>
+				<div className='image-container'>
+					<img src={`/assets/events/${ link }.jpg`} alt={ title} />
+				</div>
+			</a>
+			<div className="details">
+				<h2> <a href={`/events/${ link}`} title={ title }>{ title } </a></h2>
+				<p>
+					<span> Date: </span> { date } <br/>
+					<span> Venue: </span> { venue } <br/>
+					<span> Author: </span> { author }
+				</p>
+				<p> { description } </p>
+				<br/>
+				<a href={`/events/${ link}`} title={ title }> Read More... </a>
+			</div>
 		</div>
-	</div>
-    </TextPage>);
+	</TextPage>;
 };
 
 export default EventContainer;
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import EventContainer from '@/components/EventContainer';
 
 export default function EventPage () {
-  return (
+	return (
 		<>
 			<div id="events" style={{ display: 'none' }}>
 				<h1>Events</h1>
@@ -14,7 +14,7 @@ export default function EventPage () {
 					description="MASK's Open Campus Anime Quiz was back, and it did not disappoint! The atmosphere crackled with energy as questions flew back and forth, with each answer met by cheers and groans."
 					link="ocaq-2023"
 				/>
-	         </div>
+			</div>
 		</>
 	);
 };
