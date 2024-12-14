a82943522de8a967c24b638344de6fb323d80e24
uday-kalyan-s <udaykalyansreenivasa@gmail.com>
2024-12-15 02:02:47 +0530

    added prototype EventTemplate

---
+++ components/EventTemplate.jsx
@@ -0,0 +1,27 @@
+import React, { Component, useState } from "react";
+import style from "@/styles/EventTemplate.module.css"
+import Image from "next/image";
+
+// takes in data object
+const EventTemplate = ({ dataObj }) => {
+	return (
+        <main className={style["main"]}>
+            <Image
+                src={dataObj.url}
+                width={558}
+                height={421}
+                alt="event image"
+                className={style["image"]}
+            />
+            <div className={style["right"]}>
+                <div className={style["title"]}>{dataObj.title}</div>
+                {dataObj.content.map(text => (
+                    <div className={style["para"]} key={text}>{text}</div>
+                ))}
+                <div className={style["button"]}>Read More &rarr;</div>
+            </div>
+        </main>
+	);
+};
+
+export default EventTemplate;
