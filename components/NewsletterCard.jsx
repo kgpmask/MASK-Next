ecd2eb62613ece4ac8884b569cb97f7c569d1841
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2024-12-18 12:50:35 +0530

    added hover effect and onClick prop to redirect

--- components/NewsletterCard.jsx
+++ components/NewsletterCard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "@/styles/NewsletterCard.module.css";
 
-const NewsletterCard = ({ image, title, description }) => {
+const NewsletterCard = ({ image, title, description, link }) => {
+  const handleCardClick = () => {
+    window.location.href = link;
+  };
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleCardClick}>
       <div className={styles.imagecontainer}>
         <img src={image} alt={title} className={styles.image} />
         <div className={styles.content}>
@@ -21,17 +25,20 @@ const NewsletterCardGallery = () => {
       <NewsletterCard
         image="https://via.placeholder.com/350x250.png?text=Event+1"
         title="March 2024 Issue"
-        description="The March Edition of the issue is all about action anime! Get to know about different..."
+        description="The March Edition of the issue is all about action anime!"
+        link="https://example.com/march-2024-issue"
       />
       <NewsletterCard
         image="https://via.placeholder.com/350x250.png?text=Event+2"
         title="Event 2"
         description="This is a description for Event 2."
+        link="https://example.com/event-2"
       />
       <NewsletterCard
         image="https://via.placeholder.com/350x250.png?text=Event+3"
         title="Event 3"
         description="This is a description for Event 3."
+        link="https://example.com/event-3" 
       />
     </div>
   );
