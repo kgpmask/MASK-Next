38e64523db9af5e981ba5a8bb2299e96e146360e
Joshua Raj <joshuarajtadi@gmail.com>
2024-12-19 22:16:12 +0530

    added 20 videos to test the loading speed

--- pages/index.js
+++ pages/index.js
@@ -42,27 +42,26 @@ export default function Home() {
 						</li>
 						<li>Save and see your changes instantly.</li>
 					</ol>
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
+					<VideoCard url='https://youtu.be/iERYAx3iipw' />
+					<VideoCard url='https://youtu.be/YhVlV_dMoxg' />
 					<VideoCard url='https://youtu.be/D2rnH9XqwvA' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
-					<VideoCard url='https://www.youtube.com/watch?v=YhVlV_dMoxg' />
-					<VideoCard url='https://www.youtube.com/watch?v=iERYAx3iipw' />
+					<VideoCard url='https://youtu.be/PDCHDYsBDWA' />
+					<VideoCard url='https://youtu.be/VxVDJhMU6Zc' />
+					<VideoCard url='https://youtu.be/P0NxHvWz1ns' />
+					<VideoCard url='https://youtu.be/w_tkq4syNnI' />
+					<VideoCard url='https://youtu.be/HM1ld5vIqFg' />
+					<VideoCard url='https://youtu.be/f4muqjlSwYI' />
+					<VideoCard url='https://youtu.be/zJJGrPb8ymE' />
+					<VideoCard url='https://youtu.be/oXpj4H9Qrcw' />
+					<VideoCard url='https://youtu.be/unITcghHNVI' />
+					<VideoCard url='https://youtu.be/HCl4m9ojMMc' />
+					<VideoCard url='https://youtu.be/u7jJ2coeBRo' />
+					<VideoCard url='https://youtu.be/GX7TAigwZPw' />
+					<VideoCard url='https://youtu.be/S1pFkcsuMYE' />
+					<VideoCard url='https://youtu.be/yhtxX3JkShI' />
+					<VideoCard url='https://youtu.be/0TJkcTWEu9c' />
+					<VideoCard url='https://youtu.be/9W4eyQ7LP7g' />
+					<VideoCard url='https://youtu.be/swFVafJvVac' />
 					<div className={styles.ctas}>
 						<a
 							className={styles.primary}
