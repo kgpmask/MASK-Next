f031352a7477d3895e5c5b1e6790e921a3e88f26
Joshua Raj <joshuarajtadi@gmail.com>
2024-12-21 16:31:19 +0530

    reverted changes in index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Example from "@/components/Example";
-import VideoCard from "@/components/VideoCard";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -42,26 +41,6 @@ export default function Home() {
 						</li>
 						<li>Save and see your changes instantly.</li>
 					</ol>
-					<VideoCard url='https://youtu.be/iERYAx3iipw' />
-					<VideoCard url='https://youtu.be/YhVlV_dMoxg' />
-					<VideoCard url='https://youtu.be/D2rnH9XqwvA' />
-					<VideoCard url='https://youtu.be/PDCHDYsBDWA' />
-					<VideoCard url='https://youtu.be/VxVDJhMU6Zc' />
-					<VideoCard url='https://youtu.be/P0NxHvWz1ns' />
-					<VideoCard url='https://youtu.be/w_tkq4syNnI' />
-					<VideoCard url='https://youtu.be/HM1ld5vIqFg' />
-					<VideoCard url='https://youtu.be/f4muqjlSwYI' />
-					<VideoCard url='https://youtu.be/zJJGrPb8ymE' />
-					<VideoCard url='https://youtu.be/oXpj4H9Qrcw' />
-					<VideoCard url='https://youtu.be/unITcghHNVI' />
-					<VideoCard url='https://youtu.be/HCl4m9ojMMc' />
-					<VideoCard url='https://youtu.be/u7jJ2coeBRo' />
-					<VideoCard url='https://youtu.be/GX7TAigwZPw' />
-					<VideoCard url='https://youtu.be/S1pFkcsuMYE' />
-					<VideoCard url='https://youtu.be/yhtxX3JkShI' />
-					<VideoCard url='https://youtu.be/0TJkcTWEu9c' />
-					<VideoCard url='https://youtu.be/9W4eyQ7LP7g' />
-					<VideoCard url='https://youtu.be/swFVafJvVac' />
 					<div className={styles.ctas}>
 						<a
 							className={styles.primary}
