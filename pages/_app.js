0aef9c218316d98b000c189142b9ec4022e501c7
Mourya Grandhi <149180569+Mouryagrandhi@users.noreply.github.com>
2024-12-21 09:59:27 +0530

    addition of main content place ,for checking purpose given a fixed height of 1000px

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-	// return <Component {...pageProps} />;
 	return (
-		<>
-		<Navbar/>
-		<Footer/>
-		</>	
+	<div className="container">
+      <Navbar />
+      <main className="mainContent">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>	
 	);
 }
