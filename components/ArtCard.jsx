0048e208c87a7472071261a64dfc00e89e538c51
Ajayendra2705 <ajayendrabansod2705@gmail.com>
2025-01-21 00:35:05 +0530

    added onclick prop

--- components/ArtCard.jsx
+++ components/ArtCard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "@/styles/ArtCard.module.css";
 
-const ArtCard = ({ image, title, description }) => {
+const ArtCard = ({ image, title, description, link }) => {
+  const handleCardClick = () => {
+    window.location.href = link;
+  };
+
   return (
-    <div className={styles.card}>
+    <div className={styles.card} onClick={handleCardClick}>
       <div className={styles.imagecontainer}>
         <img src={image} alt={title} className={styles.image} />
         <div className={styles.hovercontent}>
@@ -24,16 +28,19 @@ export default ArtCard;
 //         image="/temporary_image.png"
 //         title="Bidoof"
 //         description="Ajayendra Kumar."
+//         link="https://example.com/march-2024-issue"
 //       />
 //       <ArtCard
 //         image="https://via.placeholder.com/350x250.png?text=Event+2"
 //         title="Event 2"
 //         description="This is a description for Event 2."
+//         link="https://example.com/march-2024-issue"
 //       />
 //       <ArtCard
 //         image="/temporary_image.png"
 //         title="Bidoof"
 //         description="Ajayendra Kumar."
+//         link="https://example.com/march-2024-issue"
 //       />
 //     </div>
 //   );
