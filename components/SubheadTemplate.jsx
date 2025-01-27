363488b9ea7d0ee1d81743c661ca825fb0a34184
uday-kalyan-s <udaykalyansreenivasa@gmail.com>
2025-01-27 22:19:16 +0530

    draft subhead template

---
+++ components/SubheadTemplate.jsx
@@ -0,0 +1,24 @@
+import React, { Component, useContext, useEffect} from "react";
+import style from "@/styles/ArtTemplate.module.css"
+import Image from "next/image";
+
+// takes in data objectz
+const SubheadTemplate = ({ dataObj }) => {
+	return (
+        <div className={style["element"]}>
+            <Image
+                src={dataObj.url}
+                width={558}
+                height={421}
+                alt="art image"
+                className={style["image"]}
+            />
+            <div className={style["desc"]}>
+                <div className={style["title"]}>{dataObj.artName}</div>
+                <div className={style["artist"]}>{dataObj.artist}</div>
+            </div>
+        </div>
+	);
+};
+
+export default SubheadTemplate;
