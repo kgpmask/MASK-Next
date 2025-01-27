2fdfe915db4d163a158b08f65c3a0f7a9e655099
Joshua Raj <joshuarajtadi@gmail.com>
2025-01-27 16:23:28 +0530

    add videos to array

--- pages/videos.jsx
+++ pages/videos.jsx
@@ -1,30 +1,36 @@
 import styles from '@/styles/VideoPage.module.css';
 import VideoCard from '@/components/VideoCard';
 
+let videos = [
+    { url: 'https://youtu.be/iERYAx3iipw' },
+    { url: 'https://youtu.be/YhVlV_dMoxg' },
+    { url: 'https://youtu.be/D2rnH9XqwvA' },
+    { url: 'https://youtu.be/PDCHDYsBDWA' },
+    { url: 'https://youtu.be/VxVDJhMU6Zc' },
+    { url: 'https://youtu.be/P0NxHvWz1ns' },
+    { url: 'https://youtu.be/w_tkq4syNnI' },
+    { url: 'https://youtu.be/HM1ld5vIqFg' },
+    { url: 'https://youtu.be/f4muqjlSwYI' },
+    { url: 'https://youtu.be/zJJGrPb8ymE' },
+    { url: 'https://youtu.be/oXpj4H9Qrcw' },
+    { url: 'https://youtu.be/unITcghHNVI' },
+    { url: 'https://youtu.be/HCl4m9ojMMc' },
+    { url: 'https://youtu.be/u7jJ2coeBRo' },
+    { url: 'https://youtu.be/GX7TAigwZPw' },
+    { url: 'https://youtu.be/S1pFkcsuMYE' },
+    { url: 'https://youtu.be/yhtxX3JkShI' },
+    { url: 'https://youtu.be/0TJkcTWEu9c' },
+    { url: 'https://youtu.be/9W4eyQ7LP7g' },
+    { url: 'https://youtu.be/swFVafJvVac' },
+]
+
 export default function VideoPage() {
     return (
         <>
             <div className={styles['container-video']}>
-                <VideoCard url='https://youtu.be/iERYAx3iipw' />
-                <VideoCard url='https://youtu.be/YhVlV_dMoxg' />
-                <VideoCard url='https://youtu.be/D2rnH9XqwvA' />
-                <VideoCard url='https://youtu.be/PDCHDYsBDWA' />
-                <VideoCard url='https://youtu.be/VxVDJhMU6Zc' />
-                <VideoCard url='https://youtu.be/P0NxHvWz1ns' />
-                <VideoCard url='https://youtu.be/w_tkq4syNnI' />
-                <VideoCard url='https://youtu.be/HM1ld5vIqFg' />
-                <VideoCard url='https://youtu.be/f4muqjlSwYI' />
-                <VideoCard url='https://youtu.be/zJJGrPb8ymE' />
-                <VideoCard url='https://youtu.be/oXpj4H9Qrcw' />
-                <VideoCard url='https://youtu.be/unITcghHNVI' />
-                <VideoCard url='https://youtu.be/HCl4m9ojMMc' />
-                <VideoCard url='https://youtu.be/u7jJ2coeBRo' />
-                <VideoCard url='https://youtu.be/GX7TAigwZPw' />
-                <VideoCard url='https://youtu.be/S1pFkcsuMYE' />
-                <VideoCard url='https://youtu.be/yhtxX3JkShI' />
-                <VideoCard url='https://youtu.be/0TJkcTWEu9c' />
-                <VideoCard url='https://youtu.be/9W4eyQ7LP7g' />
-                <VideoCard url='https://youtu.be/swFVafJvVac' />
+                {videos.map((video) => (
+                    <VideoCard url={video.url} />
+                ))}
             </div>
         </>
     );
