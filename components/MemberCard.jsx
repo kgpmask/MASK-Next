439a66a485b80a2e11c0752fec9dc40c900ee4e3
Ankan <93087057+ItsAnkan@users.noreply.github.com>
2025-02-08 13:23:19 +0530

    member card fixes

--- components/MemberCard.jsx
+++ components/MemberCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import style from "@/styles/MemberCard.module.css"
+import Image from "next/image";
+import { Cabin } from "next/font/google";
 
 import { BiMoviePlay } from "react-icons/bi";
 import { FaPaintbrush } from "react-icons/fa6";
 import { TfiWrite } from "react-icons/tfi";
 import { PiListChecksBold } from "react-icons/pi";
 import { FaCode } from "react-icons/fa6";
-import Image from "next/image";
+
+const cabin = Cabin({ subsets: ['latin'] });
 
 const Icon = ({ icon, description, modifier }) => {
 	return (
@@ -14,7 +17,7 @@ const Icon = ({ icon, description, modifier }) => {
 			{modifier ?
 				React.cloneElement(icon, { color: modifier === 'S' ? '#30b868' : '#e8ce3d' })
 				:
-				icon
+				React.cloneElement(icon, { color: "white" })
 			}
 		</span>
 	);
@@ -54,12 +57,12 @@ const MemberCard = ({ profilePicture, name, teams, position }) => {
 
 	return (
 		<>
-			<div className={style["member-container"]}>
+			<div className={`${style["member-container"]} ${cabin.className}`}>
 				<div className={style["profile-pic"]}>
 					<Image src={`/assets/members/${profilePicture}`} alt={name} width={200} height={200} />
 				</div>
 				<div className={style["info-container"]}>
-					<div>
+					<div className={style["member-info"]}>
 						<div className={style["member-name"]}>{name}</div>
 						<div className={style["member-position"]}>{roleText()}</div>
 					</div>
