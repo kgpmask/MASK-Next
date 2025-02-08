f5dd4c770daf02b94c831b8cb6fc54d74afdbfc1
Pratyay Ganguly <92927550+drcocktail@users.noreply.github.com>
2025-02-08 19:25:23 +0530

    Event Card, Hero Banner, Button and Member Card (#6)
    * first commit
    
    * slightly fucked images in eventcard, will solve in morning
    
    * fixed the design, it is pressing my buttons
    
    * Check the index.js page to see the implementation of the EventCard
    
    * Fixed the buttons, somewhat, just the color is weird
    
    * EventCard done. Buttons almost
    
    * Buttons also done
    
    * Buttons fixed
    
    * Fixed button typing, just use color = whatever to toggle between the four classes of buttons. Also done with the HeroBanner
    
    * Made HeroBanner responsive
    
    * Made the responsive EventCad justified
    
    * minor fixes for responsive design
    
    * made the event-card's responsive design work better for all screen-widths
    
    * apologies
    
    * apologies
    
    * final work done
    
    * Mobile View perfected
    
    * nothing much
    
    * some aesthetic spacing issues
    
    * member card made, will visualize later
    
    * css fixes + linting
    
    * more css fixes
    
    * linting
    
    * member card stuff, need final fixes doe
    
    * removing unimportant stuff
    
    * member card fixes
    
    * cleaning up
    
    * why was this here
    
    * some fixes
    
    ---------
    
    Co-authored-by: Ankan <93087057+ItsAnkan@users.noreply.github.com>

---
+++ components/EventCard.jsx
@@ -0,0 +1,37 @@
+
+
+import React from 'react';
+import Image from 'next/image';
+import { Cabin } from 'next/font/google';
+import styles from '../styles/EventCard.module.css';
+import Button from './Button';
+
+const cabin = Cabin({ subsets: ['latin'] });
+
+export default function EventCard({ event }) {
+	return (
+		<div className={`${styles.eventCard} ${cabin.className}`}>
+			{/* <div className={styles.contentWrapper}> */}
+			<div className={styles.imageWrapper}>
+				<Image src={event.image} alt={event.title} width={512} height={290} className={styles.eventImage} />
+			</div>
+			<div className={styles.textContainer}>
+				<div className={styles.textWrapper}>
+					<h3 className={styles.eventTitle}>{event.title}</h3>
+					<div className={styles.eventMeta}>
+						<p>Date: {event.date}</p>
+						<p>Venue: {event.venue}</p>
+						<p>By: {event.author}</p>
+					</div>
+					<p className={styles.eventDescription}>
+						{event.description}
+					</p>
+				</div>
+				<div className={styles.buttonWrapper}>
+					<Button text="Read More" url="/" />
+				</div>
+			</div>
+			{/* </div> */}
+		</div>
+	);
+}
---
+++ components/MemberCard.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import style from "@/styles/MemberCard.module.css"
+import Image from "next/image";
+import { Cabin } from "next/font/google";
+
+import { BiMoviePlay } from "react-icons/bi";
+import { FaPaintbrush } from "react-icons/fa6";
+import { TfiWrite } from "react-icons/tfi";
+import { PiListChecksBold } from "react-icons/pi";
+import { FaCode } from "react-icons/fa6";
+
+const cabin = Cabin({ subsets: ['latin'] });
+
+const Icon = ({ icon, description, modifier }) => {
+	return (
+		<span title={description}>
+			{modifier ?
+				React.cloneElement(icon, { color: modifier === 'S' ? '#30b868' : '#e8ce3d' })
+				:
+				React.cloneElement(icon, { color: "white" })
+			}
+		</span>
+	);
+
+}
+
+const MemberCard = ({ profilePicture, name, teams, position }) => {
+	/* We assume that the teams is of the form ["nS", "w", "q"] */
+	const teamDict = {
+		"a": "AMV",
+		"d": "Design and Arts",
+		"n": "Media and Newsletter",
+		"q": "Quiz",
+		"w": "Web Development"
+	};
+
+	const iconMap = {
+		"a": <BiMoviePlay />,
+		"d": <FaPaintbrush />,
+		"n": <TfiWrite />,
+		"q": <PiListChecksBold />,
+		"w": <FaCode />
+	}
+	
+	const roleText = () => {
+		if (position === "Governor") return "Governor";
+		else {
+			for (let team of teams) {
+				if (team.length === 2) {
+					if (team[1] === "H") return teamDict[team[0]] + " Head";
+					else if (team[1] === "S") return teamDict[team[0]] + " Sub-Head";
+				}
+			}
+			return position;
+		}
+	}
+
+	return (
+		<>
+			<div className={`${style["member-container"]} ${cabin.className}`}>
+				<div className={style["profile-pic"]}>
+					<Image src={`/assets/members/${profilePicture}`} alt={name} width={200} height={200} />
+				</div>
+				<div className={style["info-container"]}>
+					<div className={style["member-info"]}>
+						<div className={style["member-name"]}>{name}</div>
+						<div className={style["member-position"]}>{roleText()}</div>
+					</div>
+					<div className={style["team-list"]}>
+						{teams.map((team, index) => (
+							<Icon key={index} icon={iconMap[team[0]]} description={teamDict[team[0]]} modifier={team.length === 2 ? team[1] : false} />
+						))}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default MemberCard;
