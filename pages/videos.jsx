7a0206ad54b3345c6f3facb155e1aa524c77ab5f
Joshua Raj <joshuarajtadi@gmail.com>
2025-02-09 17:22:37 +0530

    add hero banner

--- pages/videos.jsx
+++ pages/videos.jsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/VideoPage.module.css';
 import VideoCard from '@/components/VideoCard';
+import HeroBanner from '@/components/HeroBanner';
 
 let videos = [
     { url: 'https://youtu.be/iERYAx3iipw' },
@@ -27,6 +28,12 @@ let videos = [
 export default function VideoPage() {
     return (
         <>
+            <HeroBanner 
+                heroTitle={'Check out our Anime Music Videos'} 
+                heroContent={'We feature a diverse range of work from talented artists within our society. From traditional  to digital art, each piece reflects unique creativity and vision. '} 
+                buttonContent={'Check out our Youtube Channel'} 
+                buttonURL={'https://www.youtube.com/@maskiitkgp'}
+            />
             <div className={styles['container-video']}>
                 {videos.map((video) => (
                     <VideoCard url={video.url} />
