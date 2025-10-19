91c2485c67b81adcd25b0227fb6888cbdd748ed2
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-10-19 18:08:24 +0530

    Created the caurosel for the Team Sub-Heads section.

--- pages/members.jsx
+++ pages/members.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FaChevronRight, FaAngleLeft } from "react-icons/fa6";
+import Carousel from 'react-simply-carousel';
 import styles from '@/styles/MembersPage.module.css';
 import MemberCard from '@/components/MemberCard';
 
@@ -17,7 +20,20 @@ const members = {
 	]
 }
 
+const subTeamHeads = [
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 1', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 2', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 3', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 4', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 5', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 6', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 7', teams: ['a', 'w'] },
+	{ profilePicture: '24_joshua.webp', name: 'Joshua Raj 8', teams: ['a', 'w'] },
+];
+
 export default function MembersPage() {
+	const [subTeamHeadsCourselIdx, setSubTeamHeadsCourselIdx] = useState(0);
+
 	return (
 		<>
 			<div className={styles['members']}>
@@ -29,6 +45,31 @@ export default function MembersPage() {
 						</div>
 					</>
 				))}
+				<h2>Team Sub-Heads</h2>
+				<div className={styles['team-sub-heads-carousel']}>
+					<Carousel
+						activeSlideIndex={subTeamHeadsCourselIdx}
+						onRequestChange={setSubTeamHeadsCourselIdx}
+						forwardBtnProps={{
+							children: <FaChevronRight />,
+							style: {
+								right: 0,
+							}
+						}}
+						backwardBtnProps={{
+							children: <FaAngleLeft />
+						}}
+						infinite={false}
+						centerMode
+						speed={700}
+					>
+						{subTeamHeads.map(member => (
+							<div className={styles['team-sub-head-member-card']}>
+								<MemberCard {...member} position="Team Sub-Heads" />
+							</div>
+						))}
+					</Carousel>
+				</div>
 			</div>
 		</>
 	);
