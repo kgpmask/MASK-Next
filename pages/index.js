95a306432dd7e46df3b1d2e7609c4c4d470bd104
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-11-09 22:46:27 +0530

    Moved recent events into a separate component.

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,16 @@
 import styles from "@/styles/home/Home.module.css";
-import RecentEventsCarousel from "../components/home/RecentEventsCarousel";
-import recentContent from "@/data/recentEvents.json";
-import { useState } from "react";
 import HeroSection from "@/components/home/HeroSection";
+import RecentEventsSection from "@/components/home/RecentEventsSection";
 import FanartSection from "@/components/home/FanartSection";
 import AboutUsSection from "@/components/home/AboutUsSection";
-import RecentEvents from "@/components/home/RecentEvents";
-import RecentEventsCarouselCard from "@/components/home/RecentEventsCarouselCard";
 
 export default function Home() {
-  const recentItems = recentContent.slice(0, 5);
-  const [currentItemIndex, setCurrentItemIndex] = useState(0);
   return (
     <div className={styles.home}>
       <HeroSection />
-      <RecentEvents
-        content={recentItems.slice(0, 3).map((recentItem) => ({
-          ...recentItem,
-          description:
-            recentItem.description.slice(0, 100) +
-            (recentItem.description.length > 100 ? "..." : ""),
-        }))}
-        currentItemIndex={currentItemIndex}
-        onClickItem={setCurrentItemIndex}
-      />
-      <div className={styles["header-content"]}>
-        <RecentEventsCarousel
-          Template={RecentEventsCarouselCard}
-          numPerPage={1}
-          discrete={false}
-          data={recentItems}
-          onSlideChange={setCurrentItemIndex}
-          currentElement={currentItemIndex}
-        />
-      </div>
+      <RecentEventsSection />
       <FanartSection />
-      <div className={styles["header-content"]}>
+      {/* <div className={styles["header-content"]}>
         <RecentEventsCarousel
           Template={RecentEventsCarouselCard}
           numPerPage={1}
@@ -45,7 +20,7 @@ export default function Home() {
           onSlideChange={setCurrentItemIndex}
           currentElement={currentItemIndex}
         />
-      </div>
+      </div> */}
       <AboutUsSection />
     </div>
   );
