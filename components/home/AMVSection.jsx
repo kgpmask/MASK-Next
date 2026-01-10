43aedd82cb0744b0ae1883a5dabab022e618ba88
Jx-ls <joshuarajtadi@gmail.com>
2026-01-11 02:25:23 +0530

    replacing this carousel with the unified carousel

--- components/home/AMVSection.jsx
+++ components/home/AMVSection.jsx
@@ -1,7 +1,6 @@
-import { useRef, useState, useEffect } from 'react';
-import Image from 'next/image';
 import styles from '@/styles/home/Home.module.css';
 import dynamic from 'next/dynamic';
+import Carousel from '../Carousel';
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 const AMVVideoItems = [
@@ -16,92 +15,31 @@ const AMVVideoItems = [
 	}
 ];
 
-export default function AMVSection () {
-	const sliderRef = useRef(null);
-	const [currentAMVVideoIndex, setCurrentAMVVideoIndex] = useState(0);
-	const [playingStates, setPlayingStates] = useState(
-		AMVVideoItems.map(() => false)
+const VideoCarouselCard = ({ dataObj }) => {
+
+	return (
+		<div className={styles['player-wrapper']}>
+			<ReactPlayer
+				className={styles['react-player']}
+				url={dataObj.url}
+				width='100%'
+				height='100%'
+				controls={true}
+				light={true}
+				playing={true}
+			/>
+		</div>
 	);
+};
 
-	const setPlayingState = (idx, state) => {
-		setPlayingStates((playingStates) => {
-			playingStates[idx] = state;
-			return playingStates;
-		});
-	};
-
-	useEffect(() => {
-		const target = sliderRef.current.children[currentAMVVideoIndex];
-		sliderRef.current.scrollTo({
-			left: target.offsetLeft - sliderRef.current.offsetLeft,
-			behavior: 'smooth'
-		});
-		setPlayingStates((playingStates) => playingStates.map(() => false));
-		if (setCurrentAMVVideoIndex) {
-			setCurrentAMVVideoIndex(currentAMVVideoIndex);
-		}
-	}, [currentAMVVideoIndex]);
-
-	const moveNext = () =>
-		setCurrentAMVVideoIndex((cur) => (cur + 1) % AMVVideoItems.length);
-	const movePrev = () =>
-		setCurrentAMVVideoIndex(
-			(cur) => (cur - 1 + AMVVideoItems.length) % AMVVideoItems.length
-		);
-	const moveHere = (num) => setCurrentAMVVideoIndex(num);
-
+export default function AMVSection () {
 	return (
 		<div className={styles['amv-section']}>
 			<h1>Anime Music Videos</h1>
-			<div className={styles['amv-container']}>
-				<Image
-					src="/assets/icons/left-arrow.svg"
-					alt="left arrow"
-					width={45}
-					height={45}
-					className={styles['amv-nav-arrow']}
-					onClick={movePrev}
-				/>
-				<div className={styles['amv-video-carousel']} ref={sliderRef}>
-					{AMVVideoItems.map((amvVideo, idx) =>
-						<div key={idx} className={styles['amv-video']}>
-							<ReactPlayer
-								url={amvVideo.url}
-								width="100%"
-								height="100%"
-								controls={true}
-								playing={playingStates[idx]}
-								onPlay={() => setPlayingState(idx, true)}
-								onPause={() => setPlayingState(idx, false)}
-							/>
-						</div>
-					)}
-				</div>
-				<Image
-					src="/assets/icons/right-arrow.svg"
-					alt="right arrow"
-					width={45}
-					height={45}
-					className={styles['amv-nav-arrow']}
-					onClick={moveNext}
-				/>
-			</div>
-			<div className={styles['amv-navigation-dots']}>
-				{Array.from({ length: AMVVideoItems.length }).map((_, num) =>
-					!(num >= currentAMVVideoIndex && num < currentAMVVideoIndex + 1) ?
-						<div
-							className={styles.dot}
-							key={num}
-							onClick={() => moveHere(num)}
-						></div>
-						:
-						<div
-							className={`${styles.dot} ${styles['active-dot']}`}
-							key={num}
-						></div>
-
-				)}
-			</div>
+			<Carousel
+				data={AMVVideoItems}
+				Card={VideoCarouselCard}
+			/>
 		</div>
 	);
 }
