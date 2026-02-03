b7191e4ce4d93e24f69c2722395a8cfaa46d37c9
Arpit Chakladar <arpitchakladar@proton.me>
2026-02-03 22:29:53 +0530

    Fetching artworks for home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,39 @@ import FanartSection from '@/components/home/FanartSection';
 import AMVSection from '@/components/home/AMVSection';
 import AboutUsSection from '@/components/home/AboutUsSection';
 
-export default function Home () {
+export async function getStaticProps () {
+	await connectDatabase();
+	let artworksRaw = await Post.find(
+		{ type: 'art' },
+		{ _id: 0, metadata: 0, type: 0, page: 0, hype: 0, __v: 0 }
+	).sort({ date: -1 }).limit(6).lean();
+
+	const artworks = artworksRaw.map(item => {
+		const d = item.date instanceof Date ? item.date : new Date(item.date);
+		return {
+			src: `/assets/art/${item.link}`,
+			year: String(d.getFullYear()),
+			author: item.attr?.join(', ') || '',
+			description: item.name,
+			season: getSeason(d.getMonth())
+		};
+	});
+
+	return {
+		props: { artworks },
+
+		revalidate: process.env.NODE_ENV === 'production'
+			? 60 * 60
+			: 1
+	};
+}
+
+export default function Home ({ artworks }) {
 	return (
 		<div className={styles.home}>
 			<HeroSection />
 			<RecentEventsSection />
-			<FanartSection />
+			<FanartSection artworks={artworks} />
 			<AMVSection />
 			<AboutUsSection />
 		</div>
